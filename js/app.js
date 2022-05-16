043e074e86e9dcd90190d47e6f033d1bf800d4b3
Sebastian <sebann231@gmail.com>
2022-05-16 19:34:20 +0200

    Elements table

--- js/app.js
+++ js/app.js
@@ -1,80 +1,33 @@
-var startPoint = "x";
-var remis = 0;
-var p = [1,2,3,4,5,6,7,8,9];
-var wygrana = []
-table.addEventListener("click",clickField);
-function clickField(e){
-    if (e.target.innerHTML == ""){
-        remis++;
-        e.target.innerHTML = startPoint;
-        if (startPoint == "x"){
-            startPoint = "o";
-        }
-        else {
-            startPoint = "x";
-    } 
+var pierwiastki = [] ;
+//console.log(wylosowany);
+var y = 0 ;
+container = document.getElementsByClassName("symbol");
+//console.log(container.length);
+
+for(x = 0; x < container.length; x++){
+  if(container[x].textContent != "DE" 
+  && container[x].textContent != "DEL" 
+  && container[x].textContent != "57-71" 
+  && container[x].textContent != "89-103"  ){
+      pierwiastki[y] = container[x].textContent ;
+      y++ ;
+  }
 }
-for(x=1 ;x< 10 ; x++ ){
-    p[x] = document.getElementById("p"+x).innerHTML;
+for(x = 0; x < pierwiastki.length; x++){
+//console.log("x:"+x+","+pierwiastki[x]);
 }
-function getElementP(elem,what){
-    document.getElementById("p"+elem).innerHTML = '<div style="color:lime;">'+what+'</div>'
-}
-function getResult(info){
-    document.getElementById("info").innerHTML = info;
-}
-function wygranaGry(x,kto,par1,par2,par3){
-    // x - unikalny identyfikator,
-    // jaka kofiguracja wygrywa,
-    // par1,par2,par3 - pola, które wygrywają
-wygrana[x]=p[par1] + p[par2] + p[par3];
-if(wygrana[x] == kto){
-    if(kto == "xxx"){
-        getElementP(par1,"x")
-        getElementP(par2,"x")
-        getElementP(par3,"x")
-        getResult('<h2 style="color:white;">WYGRAŁY KRZYŻYKI</h2>');
-        document.getElementById("resetButton").className = "visible buttonReset  btn btn-secondary";
-        table.removeEventListener("click",clickField)
+
+random = Math.floor(Math.random() * pierwiastki.length) ;
+console.log(pierwiastki[random]);
+
+for(x = 0 ; x < container.length ; x++){
+  if(container[x].textContent == pierwiastki[random]){
+     wylosowany = container[x] ;
     }
-    if(kto == "ooo"){
-        getElementP(par1,"o")
-        getElementP(par2,"o")
-        getElementP(par3,"o")
-        getResult('<h2 style="color:white;">WYGRAŁY KÓŁKA</h2>');
-        document.getElementById("resetButton").className = "visible buttonReset  btn btn-secondary";
-        table.removeEventListener("click",clickField)
-    }
-    resetTable.disabled = false;
 }
-}
-wynik = []
-wynik[1] = "xxx";
-wynik[2] = "ooo";
-for(x=1;x<3;x++){
-    wygranaGry(1,wynik[x],1,2,3);
-    wygranaGry(2,wynik[x],4,5,6);
-    wygranaGry(3,wynik[x],7,8,9);
-    wygranaGry(4,wynik[x],1,4,7);
-    wygranaGry(5,wynik[x],2,5,8);
-    wygranaGry(6,wynik[x],3,6,9);
-    wygranaGry(7,wynik[x],1,5,9);
-    wygranaGry(8,wynik[x],3,5,7);
-}
-if(remis == 9){
-    document.getElementById("resetButton").className = "visible buttonReset  btn btn-secondary";
-    table.removeEventListener("click",clickField);
-    document.getElementById("info").innerHTML = '<h2 style="color:white;">Remis!!!</h2>';
-}
-}
-resetButton.addEventListener("click",resetTable)
-function resetTable(e){
-    for(x=1 ;x< 10 ; x++ ){
-        p[x] = document.getElementById("p"+x).innerHTML = "";
-    }
-    document.getElementById("info").innerHTML = "";
-    document.getElementById("resetButton").className = "hide buttonReset  btn btn-secondary";
-    table.addEventListener("click",clickField);
-    startPoint = "x";
-    remis = 0;
-}
+
+console.log(wylosowany);
+wylosowany = wylosowany.parentNode ;
+wylosowany.setAttribute("class","checked");
+
+
