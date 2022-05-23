c58a2c26333bb9a3ef30b2953d6d521c20ff6608
Sebastian <sebann231@gmail.com>
2022-05-23 19:09:23 +0200

    resetButton, points

--- js/app.js
+++ js/app.js
@@ -1,60 +1,36 @@
-var pierwiastki = [] ;
-var elementLosed = [] ;
-var odp1 = "";
+var elementsTable = [] ;
+var elementChosed = [] ;
+var answer = "";
 var y = 0 ;
 var k=0 ;
 var point = 0;
 container = document.getElementsByClassName("symbol");
-function randomElements(){
-  y=0 ;
 for(x = 0; x < container.length; x++){
   if(container[x].textContent != "DE"
+  && container[x].textContent != "DEL"
   && container[x].textContent != "57-71" 
   && container[x].textContent != "89-103"  ){
-      pierwiastki[y] = container[x].textContent ;
+      elementsTable[y] = container[x] ;
       y++ ;
   }
 }
-for(x = 0; x < pierwiastki.length; x++){
+
+function randomElements(){
+if(elementChosed.length < elements.length){
+randomNumber = Math.floor(Math.random() * elements.length) ;
+answer = elements[randomNumber][1];
+randomElement = elementsTable[randomNumber].parentNode ;
+if (elementChosed.includes(randomNumber)){
+       randomElements();
+} else {
+randomElement.setAttribute("class","checked");
+elementChosed.push(randomNumber);
 }
-//console.log(pierwiastki.length);
-
-random = Math.floor(Math.random() * pierwiastki.length) ;
-//console.log(pierwiastki[random]);
-//console.log(random);
-odp1 = pierwiastek[random][1];
-//console.log("odp1:"+odp1);
-
-
-for(x = 0 ; x < container.length ; x++){
-  if(container[x].textContent == pierwiastki[random]){
-     wylosowany = container[x] ;
-    }
+} else {
+  document.getElementById("statictic").className = "show btn btn-warning"
+  document.getElementById("inputAuto").className = "hide"
+  document.getElementById("end").className = "show"
 }
-
-//console.log(wylosowany);
-
-wylosowany = wylosowany.parentNode ;
-
-for(z = 0; z < elementLosed.length; z++){
-
-  if (pierwiastki[random] == elementLosed[z]){
-    //wylosowany.classList.remove("checked");
-    console.log("powtorka");
-    randomElements();
-  }
-  }
- 
-  wylosowany.setAttribute("class","checked");
-elementLosed.push(pierwiastki[random]);
-//console.log(elementLosed);
-
-
-console.log(elementLosed.length);
-  
-
-
-
 }
 
 const easy = document.getElementById("easy");
@@ -66,41 +42,48 @@ medium.addEventListener('click',gameStart);
 hard.addEventListener('click',gameStart);
 
 function gameStart(e){
-  console.log(e.target.id);
   randomElements();
   document.getElementById("startButton").className = "hide"
   document.getElementById("inputAuto").className = "show"
+  document.getElementById("resetButton").className = "show btn btn-info"
 }
-
 
 function checkQuestion(event){
   if(event.keyCode == 13){
-   
-
     inputAuto = document.getElementById("inputAuto").value
-    if(inputAuto == odp1){
-      //console.log(wylosowany);
-      wylosowany.classList.remove("checked");
-      wylosowany.classList.add("goodAnswer");
+    if(inputAuto == answer){
+      randomElement.classList.remove("checked");
+      randomElement.classList.add("goodAnswer");
+      randomElement.classList.add("okres");
       point++;
-
-      randomElements();
     }
     else {
-      wylosowany.classList.remove("checked");
-      wylosowany.classList.add("badAnswer");
-      randomElements();
+      randomElement.classList.remove("checked");
+      randomElement.classList.add("badAnswer");
+      randomElement.classList.add("okres");
     }
+    statistics.innerHTML = "<h1>Liczba Punktów: "+point+"</h1>";
+    randomElements();
+    event.target.value = "";
   }
 }
 
-if(elementLosed.length == pierwiastki.length){
-  
+resetButton.addEventListener("click", resetGame);
+function resetGame(e){
+  for(x = 0; x < elements.length; x++){
+    elementsTable[x].parentNode.classList.remove("badAnswer");
+    elementsTable[x].parentNode.classList.remove("goodAnswer");
+    elementsTable[x].parentNode.classList.remove("checked");
+    elementsTable[x].parentNode.classList.remove("okresowy");
+    elementsTable[x].parentNode.classList.add("okresowy");
+  }
+  elementChosed = [] ;
+  document.getElementById("startButton").className = "btn btn-secondary show";
+  document.getElementById("inputAuto").className = "hide";
+  document.getElementById("resetButton").className = "hide";
+  document.getElementById("statictic").className = "hide";
+  document.getElementById("end").className = "hide";
+  document.getElementById("end").className = "hide";
+  point = 0;
+  statistics.innerHTML = "<h1>Liczba Punktów: "+point+"</h1>";
 }
-
-
-
-
-
-
-
